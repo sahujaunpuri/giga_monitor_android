54908c33bc88097f2ea73ca0c950962a25f8577b
Paulo_Keller <paulo.keller@zappts.com>
2018-02-09 16:57:08 -0200

    devices logout bug fixed

--- app/src/main/java/br/inatel/icc/gigasecurity/gigamonitor/activities/DeviceListActivity.java
+++ app/src/main/java/br/inatel/icc/gigasecurity/gigamonitor/activities/DeviceListActivity.java
@@ -67,31 +67,35 @@ public class DeviceListActivity extends ActionBarActivity {
 //            finish();
         }
 
-        mExpandableListView.setOnGroupClickListener(new ExpandableListView.OnGroupClickListener() {
-            @Override
-            public boolean onGroupClick(ExpandableListView parent, View v, int groupPosition, long id) {
-                if(mDeviceManager.networkType == -1)
-                    return true;
-                if (previousGroup == -1) {
-                    previousGroup = groupPosition;
-                    parent.expandGroup(groupPosition, true);
-                    return true;
-                } else if (mDeviceManager.getDeviceChannelsManagers().get(previousGroup).recCounter > 0) {
-                    Toast.makeText(mContext, "Finalize a gravação", Toast.LENGTH_SHORT).show();
-                    return true;
-                } else if(previousGroup == groupPosition){
+        try {
+            mExpandableListView.setOnGroupClickListener(new ExpandableListView.OnGroupClickListener() {
+                @Override
+                public boolean onGroupClick(ExpandableListView parent, View v, int groupPosition, long id) {
+                    if(mDeviceManager.networkType == -1)
+                        return true;
+                    if (previousGroup == -1) {
+                        previousGroup = groupPosition;
+                        parent.expandGroup(groupPosition, true);
+                        return true;
+                    } else if (mDeviceManager.getDeviceChannelsManagers().get(previousGroup).recCounter > 0) {
+                        Toast.makeText(mContext, "Finalize a gravação", Toast.LENGTH_SHORT).show();
+                        return true;
+                    } else if(previousGroup == groupPosition){
 //                    parent.collapseGroup(previousGroup);
 //                    previousGroup = -1;
-                    Log.d(TAG, "onGroupClick: ");
-                    return true;
-                } else {
-                    parent.collapseGroup(previousGroup);
-                    parent.expandGroup(groupPosition, true);
-                    previousGroup = groupPosition;
-                    return true;
+                        Log.d(TAG, "onGroupClick: ");
+                        return true;
+                    } else {
+                        parent.collapseGroup(previousGroup);
+                        parent.expandGroup(groupPosition, true);
+                        previousGroup = groupPosition;
+                        return true;
+                    }
                 }
-            }
-        });
+            });
+        } catch (Exception error) {
+            error.printStackTrace();
+        }
 
         mDeviceManager.setSharedPreferences(mContext.getSharedPreferences("state", MODE_PRIVATE));
 
