b9d38e0adb071ab48ddf528426cf507ba1caf55a
Paulo_Keller <paulo.keller@zappts.com>
2018-02-16 16:59:04 -0200

    bug fixes

--- app/src/main/java/br/inatel/icc/gigasecurity/gigamonitor/core/ConnectionReceiver.java
+++ app/src/main/java/br/inatel/icc/gigasecurity/gigamonitor/core/ConnectionReceiver.java
@@ -35,7 +35,7 @@ public class ConnectionReceiver extends BroadcastReceiver{
             } else {
                 try {
                     mDeviceManager.networkType = -1;
-                    mDeviceManager.setDevicesLogout();
+//                    mDeviceManager.setDevicesLogout();
                 } catch(Exception error) {
                     error.printStackTrace();
                 }
